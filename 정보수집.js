0f78c4389addb67f5ae919b51a866981d17a1ba2
joey914 <joey91426@gmail.com>
2017-03-01 03:38:28 +0900

    good

--- 정보수집.js
+++ 정보수집.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $('#subt').click(function() {
-        alert("clicked. ajax실행합니다.");
         var name = $("#name").val();
         var kt = $("#kt").val();
         var pn = $("#pn").val();
@@ -8,7 +7,6 @@ $(document).ready(function() {
         var nsc = $("#nsc").val();
         var wsc = $("#wsc").val();
         var btg = $("#bt").val();
-        alert(name + kt + pn + email + nsc + wsc + btg);
 
 
             var regdata = "name=" + name + "&kakaotalk=" + kt + "&Phonenum=" + pn +"&email=" + email + "&Wantscore=" + wsc +"&Nowscore=" + nsc + "&battletag=" + btg;
@@ -18,11 +16,11 @@ $(document).ready(function() {
                     data: regdata,
                     success: function(data)     {         //data - response from server
                     if (data.success == true) {
-                        alert("회원가입 성공");
+                        alert("정상적으로 접수되었습니다.");
                     }
                 },
                     error: function(xhr, status, error)     {
-                    alert("서버에 오류생겼다 bro; jqXHR : " + xhr + " status : " + status + " error : " + error);
+                    alert("서버에 문제가 있습니다. 관리자에게 문의하세요");
                 }
             });
     })
--- 정보확인.js
+++ 정보확인.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
-  /*var jsonpar;
-  var testjson = eval([{"name":"name","id":"id","pw":"pw"},{"name":"name1","id":"id1","pw":"pw1"}]);
-  for(var i=0;i < testjson.length;i++)
-  {
-    document.getElementById("table").innerHTML += "<tr><td>"+testjson[i].name+"</td><td>"+testjson[i].id+"</td><td>"+testjson[i].pw+"</td></tr>"
-  }*/
     $.ajax({        
         type: "GET",
         url: "http://soylatte.kr:3000/userdataa",
@@ -12,11 +6,11 @@ $(document).ready(function() {
           var testjson = eval(data);
             for(var i=0;i < testjson.length;i++)
             {
-              document.getElementById("table").innerHTML += "<tr><td>"+testjson[i].name+"</td><td>"+testjson[i].kakaotalk+"</td><td>"+testjson[i].email+"</td><td>"+testjson[i].Phonenum+"</td><td>"+testjson[i].email+"</td><td>"+testjson[i].Nowscore+"</td><td>"+testjson[i].Wantscore+"</td></tr>"
+              document.getElementById("table").innerHTML += "<tr><td>"+testjson[i].name+"</td><td>"+testjson[i].kakaotalk+"</td><td>"+testjson[i].email+"</td><td>"+testjson[i].Phonenum+"</td><td>"+testjson[i].Nowscore+"</td><td>"+testjson[i].Wantscore+"</td><td>"+testjson[i].battletag+"</td></tr>"
             }
         },
             error: function(xhr, status, error)     {
-            alert("서버에 오류생겼다 bro; jqXHR : " + xhr + " status : " + status + " error : " + error);
+            alert("서버에 문제가 있습니다. 관리자에게 문의하세요");
         }
     });
 });
